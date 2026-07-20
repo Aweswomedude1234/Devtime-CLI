a3f4889955f057ee2f5c384fc98c2f9aad809bc6
Aweswomedude1234 <nithilan.murugesan64949@gmail.com>
2026-07-20 10:24:21 -0400

    Script to detect claude process

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+import subprocess
+import time
+
+if "claude" in output or "claude code" in output:
+    print("Claude code running")
+
+else:
+    print("Claude Code not running")
