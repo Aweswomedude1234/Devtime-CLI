fca5c811a7f7f21d3375510aa8e2fa1a25047919
Aweswomedude1234 <nithilan.murugesan64949@gmail.com>
2026-07-20 14:21:19 -0400

    Use hook keys instead, created claude_hook.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import subprocess
 import time
+import json
 
 result = subprocess.run(["ps", "aux"], capture_output = True, text = True)
 output = result.stdout
+claudeOn = False
 for process in output.splitlines():
     if "claude" in process.lower():
-        print("Claude code running")
         print(process)
-else:
+        claudeOn = True
+
+claude = subprocess.Popen(["claude", "--output-format", "stream-json", "--input-format", "stream-json"], stdout=subprocess.PIPE, text = True)
+if claudeOn:
+    print("Claude Code Running")
+    for line in claude.stdout:
+        event = json.loads(line)
+        print(event)
+    
+else: 
     print("Claude Code not running")
+
